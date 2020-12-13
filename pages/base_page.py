3c772a8f24ebe318c10f55dd28755c7202ce2ad6
RomanGolub <romannovoselovcl@gmail.com>
2020-12-13 20:56:59 +0300

    add BasePage and MainPage

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+#базовая страница, от которой будут унаследованы все остальные классы
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+    def open(self):
+        self.browser.get(self.url)
